c1d713124f565b551f68a5dfb23458d5258f8fdc
Regi Fronda <regisfronda@gmail.com>
2021-07-13 00:09:00 -0700

    Add method fibs_rec

--- main.rb
+++ main.rb
@@ -1,17 +1,15 @@
 def fibs(number)
   i = 2
-  result = Array.new
+  result = [0]
   fibonacci_array = [0, 1]
 
-  while i <= number
-    
+  while i <= number 
     fibonacci_array[i] = fibonacci_array[i - 1] + fibonacci_array[i - 2]
-    fibonacci_array.push(fibonacci_array[i])
+    result.push(fibonacci_array[i])
     i += 1
   end
-  fibonacci_array.pop
-  fibonacci_array.pop
-  return fibonacci_array
+  return result
 end
 
-p fibs(9)
+def fibs_rec
+end
