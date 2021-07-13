aa3e24032cecb46f94321b5bb4f6b46bd2365944
Regi Fronda <regisfronda@gmail.com>
2021-07-12 23:45:31 -0700

    Add changes so method fibs takes a number and returns an array containing that many numbers from the fibonacci sequence

--- main.rb
+++ main.rb
@@ -1,2 +1,17 @@
-def fibs(number, result = [])
+def fibs(number)
+  i = 2
+  result = Array.new
+  fibonacci_array = [0, 1]
+
+  while i <= number
+    
+    fibonacci_array[i] = fibonacci_array[i - 1] + fibonacci_array[i - 2]
+    fibonacci_array.push(fibonacci_array[i])
+    i += 1
+  end
+  fibonacci_array.pop
+  fibonacci_array.pop
+  return fibonacci_array
 end
+
+p fibs(9)
